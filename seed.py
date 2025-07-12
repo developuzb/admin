10ed07f99af3da37f9810871ecee64060e54c6c3
Suxrob <suxrob@gmail.com>
2025-07-12 17:49:51 +0500

    used_cashback ustuni avtomatik qo‘shish kodi qo‘shildi

--- seed.py
+++ seed.py
@@ -1,17 +1,28 @@
 import sqlite3
+import os
 
 
 def add_used_cashback_column():
-    conn = sqlite3.connect("database.db")
-    cursor = conn.cursor()
     try:
+        BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+        db_path = os.path.join(BASE_DIR, "bot.db")
+
+        conn = sqlite3.connect(db_path)
+        cursor = conn.cursor()
+
         cursor.execute(
             "ALTER TABLE orders ADD COLUMN used_cashback INTEGER DEFAULT 0")
         conn.commit()
+        print("✅ used_cashback ustuni qo‘shildi.")
+
     except sqlite3.OperationalError as e:
         if "duplicate column name" in str(e):
-            pass  # Ustun allaqachon mavjud
+            print("ℹ️ used_cashback ustuni allaqachon mavjud.")
         else:
             raise
     finally:
         conn.close()
+
+
+# Funksiyani ishga tushirish
+add_used_cashback_column()
