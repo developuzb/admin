406efad4412410dfc0173ad75d9e94b950ce15a8
Suxrob <suxrob@gmail.com>
2025-07-14 19:21:14 +0500

    ✅ Add PATCH endpoint for updating last_order

--- seed.py
+++ seed.py
@@ -1,28 +1,8 @@
-import sqlite3
-import os
+def add_last_order_column():
+    db = sqlite3.connect("bot.db")
+    cursor = db.cursor()
+    cursor.execute("ALTER TABLE services ADD COLUMN last_order INTEGER;")
+    db.commit()
 
 
-def add_used_cashback_column():
-    try:
-        BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-        db_path = os.path.join(BASE_DIR, "bot.db")
-
-        conn = sqlite3.connect(db_path)
-        cursor = conn.cursor()
-
-        cursor.execute(
-            "ALTER TABLE orders ADD COLUMN used_cashback INTEGER DEFAULT 0")
-        conn.commit()
-        print("✅ used_cashback ustuni qo‘shildi.")
-
-    except sqlite3.OperationalError as e:
-        if "duplicate column name" in str(e):
-            print("ℹ️ used_cashback ustuni allaqachon mavjud.")
-        else:
-            raise
-    finally:
-        conn.close()
-
-
-# Funksiyani ishga tushirish
-add_used_cashback_column()
+print("yaratildi")
